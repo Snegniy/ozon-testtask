c892fc6b1e838d1c22274c205ba517b601d10237
Sergey Doronin <125103@gmail.com>
2023-06-28 12:24:56 +0300

    add test

--- internal/repository/memdb/repository_test.go
+++ internal/repository/memdb/repository_test.go
@@ -2,80 +2,52 @@ package memdb
 
 import (
 	"context"
-	"fmt"
 	"github.com/Snegniy/ozon-testtask/pkg/logger"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 var baseUrl = "ozon.ru"
-var shortenedUrl = "AAAAAAAAAA"
+var shortenUrl = "AAAAAAAAAA"
 var ctx = context.Background()
 
 func TestRepository_WriteNewLink(t *testing.T) {
 	logger.Init("no")
 	db := NewRepository()
 
-	_, err := db.WriteNewLink(ctx, baseUrl, shortenedUrl)
-	if err != nil {
-		t.Errorf("Save error. Expected: nil, got: %s", err.Error())
-	}
-	base, err := db.GetBaseURL(ctx, shortenedUrl)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if base != baseUrl {
-		t.Errorf("Incorrect response. Expected: %s, got: %s", baseUrl, base)
-	}
+	res, err := db.WriteNewLink(ctx, baseUrl, shortenUrl)
 
-	_, err = db.WriteNewLink(ctx, baseUrl, shortenedUrl)
-	if err != nil {
-		t.Errorf("Error not handled. Expected: nil, got: %s", err)
-	}
+	require.Equal(t, shortenUrl, res)
+	require.NoError(t, err)
 }
 
 func TestRepository_GetBaseURL(t *testing.T) {
 	logger.Init("no")
 	db := NewRepository()
 
-	base, err := db.GetBaseURL(ctx, shortenedUrl)
-	ourError := fmt.Errorf("short link for \"%s\" not found", shortenedUrl)
-	if err == ourError {
-		t.Fatalf("Incorrect response. Expected: %s, got: %s", ourError, err)
-	}
+	_, _ = db.WriteNewLink(ctx, baseUrl, shortenUrl)
+	res, err := db.GetBaseURL(ctx, shortenUrl)
 
-	_, err = db.WriteNewLink(ctx, baseUrl, shortenedUrl)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Equal(t, baseUrl, res)
+	require.NoError(t, err)
+}
 
-	base, err = db.GetBaseURL(ctx, shortenedUrl)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if base != baseUrl {
-		t.Errorf("Incorrect response. Expected: %s, got: %s", baseUrl, base)
-	}
+func TestRepository_GetBaseURL2(t *testing.T) {
+	logger.Init("no")
+	db := NewRepository()
+
+	_, err := db.GetBaseURL(ctx, shortenUrl)
+
+	require.Error(t, err)
 }
 
 func TestRepository_GetShortURL(t *testing.T) {
 	logger.Init("no")
 	db := NewRepository()
 
-	_, err := db.GetShortURL(ctx, baseUrl)
-	if err == nil {
-		t.Errorf("Incorrect response. Expected: false, got: %t", err)
-	}
+	_, _ = db.WriteNewLink(ctx, baseUrl, shortenUrl)
+	res, err := db.GetShortURL(ctx, baseUrl)
 
-	_, err = db.WriteNewLink(ctx, baseUrl, shortenedUrl)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	short, err := db.GetShortURL(ctx, baseUrl)
-	if err != nil {
-		t.Errorf("Incorrect response. Expected: false, got: %t", err)
-	}
-	if short != shortenedUrl {
-		t.Errorf("Incorrect response. Expected: %s, got: %s", shortenedUrl, short)
-	}
+	require.Equal(t, shortenUrl, res)
+	require.NoError(t, err)
 }
